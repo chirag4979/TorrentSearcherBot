1eaa5b9c5b9de044302f97896be98cc52153c219
chirag4979 <65808692+chirag4979@users.noreply.github.com>
2020-09-23 16:30:20 +0530

    Update torrent.py

--- helpers/torrent.py
+++ helpers/torrent.py
@@ -1,7 +1,7 @@
 import requests
 
 def torrent_search(torrent_name):
-    base_url ="https://api.sumanjay.cf/torrent/?query={torrent_name}"
+    base_url =f"https://1337x.to/usearch={torrent_name}"
     request = requests.get(base_url)
     if (request.status_code == 200):
         response = request.json()
